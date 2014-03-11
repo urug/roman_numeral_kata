99b4dff23fc20cea94fdab71285ac501c17eec79
hqmq <michael@riesd.com>
2014-03-11 16:07:29 -0600

    add tests in both directions

--- spec/roman_numeral_kata_spec.rb
+++ spec/roman_numeral_kata_spec.rb
@@ -4,19 +4,39 @@ require 'roman_numeral_kata'
 describe RomanNumeralKata do
   Given { RomanNumeralKata }
 
-  Then { convert(1).should == "I" }
-  Then { convert(2).should == "II" }
-  Then { convert(4).should == "IV" }
-  Then { convert(5).should == "V" }
-  Then { convert(8).should == "VIII" }
-  Then { convert(9).should == "IX" }
-  Then { convert(10).should == "X" }
-  Then { convert(12).should == "XII" }
-  Then { convert(14).should == "XIV" }
-  Then { convert(15).should == "XV" }
-  Then { convert(20).should == "XX" }
-  Then { convert(2013).should == "MMXIII" }
-  Then { convert(1949).should == "MCMXLIX" }
-  Then { convert(3999).should == "MMMCMXCIX" }
-  Then { convert(0).should == "MMMCMXCIX" }
+  describe ".to_roman" do
+    Then { to_roman(   1).should ==         "I" }
+    Then { to_roman(   2).should ==        "II" }
+    Then { to_roman(   4).should ==        "IV" }
+    Then { to_roman(   5).should ==         "V" }
+    Then { to_roman(   8).should ==      "VIII" }
+    Then { to_roman(   9).should ==        "IX" }
+    Then { to_roman(  10).should ==         "X" }
+    Then { to_roman(  12).should ==       "XII" }
+    Then { to_roman(  14).should ==       "XIV" }
+    Then { to_roman(  15).should ==        "XV" }
+    Then { to_roman(  20).should ==        "XX" }
+    Then { to_roman(2013).should ==    "MMXIII" }
+    Then { to_roman(1949).should ==   "MCMXLIX" }
+    Then { to_roman(3999).should == "MMMCMXCIX" }
+    Then { to_roman(   0).should == "MMMCMXCIX" }
+  end
+
+  describe ".from_roman" do
+    Then { from_roman(        "I").should ==    1 }
+    Then { from_roman(       "II").should ==    2 }
+    Then { from_roman(       "IV").should ==    4 }
+    Then { from_roman(        "V").should ==    5 }
+    Then { from_roman(     "VIII").should ==    8 }
+    Then { from_roman(       "IX").should ==    9 }
+    Then { from_roman(        "X").should ==   10 }
+    Then { from_roman(      "XII").should ==   12 }
+    Then { from_roman(      "XIV").should ==   14 }
+    Then { from_roman(       "XV").should ==   15 }
+    Then { from_roman(       "XX").should ==   20 }
+    Then { from_roman(   "MMXIII").should == 2013 }
+    Then { from_roman(  "MCMXLIX").should == 1949 }
+    Then { from_roman("MMMCMXCIX").should == 3999 }
+    Then { from_roman("MMMCMXCIX").should ==    0 }
+  end
 end
